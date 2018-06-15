62b650531c37f15700e65f322e47c4910a83e191
Spencer Pearson <suspense@cs.washington.edu>
2018-06-14 21:11:41 -0700

    refine typing

--- __init__.py
+++ __init__.py
@@ -1,10 +1,15 @@
 import copy
 
+from typing import NewType
+
 import networkx as nx
 import pydot
 
-def dataflow_graph_from_json_object(j: dict) -> nx.DiGraph:
-  result = nx.DiGraph()
+DataflowGraph = NewType('DataflowGraph', nx.DiGraph)
+DotGraph = NewType('DotGraph', nx.DiGraph)
+
+def dataflow_graph_from_json_object(j: dict) -> DataflowGraph:
+  result = DataflowGraph(nx.DiGraph())
   for (id, attrs) in j.get('nodes', {}).items():
     if attrs['type'] not in {'store', 'process'}:
       raise ValueError('node {!r} does not have type=store or type=process'.format(id))
@@ -14,14 +19,14 @@ def dataflow_graph_from_json_object(j: dict) -> nx.DiGraph:
       result.add_edge(src, dst, **attrs)
   return result
 
-def dataflow_graph_to_dot_graph(g: nx.DiGraph) -> nx.DiGraph:
+def dataflow_graph_to_dot_graph(g: DataflowGraph) -> DotGraph:
   result = copy.deepcopy(g)
   for (id, attrs) in result.nodes.items():
     if attrs['type'] == 'store':
       result.nodes[id]['shape'] = 'cylinder'
     if attrs.get('datatype'):
       result.nodes[id].setdefault('label', attrs['datatype'])
-  return result
+  return DotGraph(result)
 
-def nx_to_dot(g: nx.Graph) -> str:
+def nx_to_dot(g: DotGraph) -> str:
   return nx.nx_pydot.to_pydot(g).create_dot().decode()
