cc3af5dba82358afb7a7a6e04f7df102462d4b45
Spencer Pearson <suspense@cs.washington.edu>
2018-06-14 22:22:28 -0700

    tweak JSON dataflow format; rename function

--- __init__.py
+++ __init__.py
@@ -10,13 +10,20 @@ DotGraph = NewType('DotGraph', nx.DiGraph)
 
 def dataflow_graph_from_json_object(j: dict) -> DataflowGraph:
   result = DataflowGraph(nx.DiGraph())
-  for (id, attrs) in j.get('nodes', {}).items():
-    if attrs['type'] not in {'store', 'process'}:
-      raise ValueError('node {!r} does not have type=store or type=process'.format(id))
-    result.add_node(id, **attrs)
-  for (src, outgoings) in j.get('transfers', {}).items():
-    for (dst, attrs) in outgoings.items():
-      result.add_edge(src, dst, **attrs)
+
+  duplicate_node_ids = set(j.get('stores', {}).keys()) & set(j.get('processes', {}).keys())
+  if duplicate_node_ids:
+    raise ValueError('node ids {} are given as both stores and values'.format(duplicate_node_ids))
+  for (id, attrs) in j.get('stores', {}).items():
+    result.add_node(id, type='store', **attrs)
+  for (id, attrs) in j.get('processes', {}).items():
+    result.add_node(id, type='process', **attrs)
+
+  for (src, outgoingss) in j.get('transfers', {}).items():
+    for (dst, outgoings) in outgoingss.items():
+      for attrs in outgoings:
+        result.add_edge(src, dst, **attrs)
+
   return result
 
 def dataflow_graph_to_dot_graph(g: DataflowGraph) -> DotGraph:
@@ -28,5 +35,5 @@ def dataflow_graph_to_dot_graph(g: DataflowGraph) -> DotGraph:
       result.nodes[id].setdefault('label', attrs['datatype'])
   return DotGraph(result)
 
-def nx_to_dot(g: DotGraph) -> str:
+def graph_to_dot(g: DotGraph) -> str:
   return nx.nx_pydot.to_pydot(g).create_dot().decode()
